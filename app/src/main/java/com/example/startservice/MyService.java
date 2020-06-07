adab7ce46f3e5648f7d64b92f6aaac3b4ffa610a
doudou <allenwang_scda@163.com>
2020-06-07 16:16:56 +0800

    StartService

---
+++ app/src/main/java/com/example/startservice/MyService.java
@@ -0,0 +1,9 @@
+package com.example.startservice;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+public class MyService {
+}
